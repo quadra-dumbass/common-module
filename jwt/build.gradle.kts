0cd5a52e43e3ca4449c112bf35e35763a92d5ed9
rymph <rymph0501@gmail.com>
2025-08-18 16:02:34 +0900

    build.gradle.kts modified

--- jwt/build.gradle.kts
+++ jwt/build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
-    id("com.quadra.spring-library-conventions") version "1.0.1"
+    id("com.quadra.spring-library-conventions") version "1.0.2"
 }
 
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
 
-    implementation("org.springframework.boot:spring-boot-starter")
-
+    implementation("org.springframework.boot:spring-boot")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
+    implementation("org.springframework:spring-core")
+    implementation("org.springframework:spring-context")
     // Used to implement jwt-related functionality
     implementation("com.auth0:java-jwt:4.4.0")
 }
